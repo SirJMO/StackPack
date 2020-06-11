252317ba5af6599cae4cee11f38312a65eadb12e
SirJMO <omfgrichard@hotmail.com>
2020-06-11 02:00:28 +0200

    Add files via upload

---
+++ prototypes/item.lua
@@ -0,0 +1,12 @@
+data:extend(
+{
+  {
+    type = "item",
+    name = "stack-pack",
+    icon = "__StackPack__/graphics/icons/stack-pack.png",
+    icon_size = 64, icon_mipmaps = 4,
+    subgroup = "tool",
+    order = "a[items]-z",
+    stack_size = 404
+  }
+})
